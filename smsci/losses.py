41d69b4f57097393080ca415ea8fec8514ec50bc
anonyme-anonymee <159822306+anonyme-anonymee@users.noreply.github.com>
2024-03-06 14:43:28 +0100

    Update losses.py

--- smsci/losses.py
+++ smsci/losses.py
@@ -14,7 +14,6 @@ def bce_loss(input, target):
     loss = input.clamp(min=0) - input * target + (1 + neg_abs.exp()).log()
     return loss.mean()
 
-
 def gan_g_loss(scores_fake):
     """
     Input:
@@ -25,7 +24,6 @@ def gan_g_loss(scores_fake):
     """
     y_fake = torch.ones_like(scores_fake) * random.uniform(0.7, 1.2)
     return bce_loss(scores_fake, y_fake)
-
 
 def gan_d_loss(scores_real, scores_fake):
     """
@@ -41,7 +39,6 @@ def gan_d_loss(scores_real, scores_fake):
     loss_real = bce_loss(scores_real, y_real)
     loss_fake = bce_loss(scores_fake, y_fake)
     return loss_real + loss_fake
-
 
 def l2_loss(pred_traj, pred_traj_gt, loss_mask, random=0, mode='average'):
     """
@@ -64,7 +61,6 @@ def l2_loss(pred_traj, pred_traj_gt, loss_mask, random=0, mode='average'):
         return torch.sum(loss) / torch.numel(loss_mask.data)
     elif mode == 'raw':
         return loss.sum(dim=2).sum(dim=1)
-
 
 def displacement_error(pred_traj, pred_traj_gt, consider_ped=None, mode='sum'):
     """
@@ -89,7 +85,6 @@ def displacement_error(pred_traj, pred_traj_gt, consider_ped=None, mode='sum'):
         return torch.sum(loss)
     elif mode == 'raw':
         return loss
-
 
 def final_displacement_error(pred_pos, pred_pos_gt, consider_ped=None, mode='sum'):
     """
